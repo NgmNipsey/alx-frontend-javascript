ae85ce61221d09e4b611b19238aed522759c4414
Luka Korok Kuro <lukakorokkuro72334@gmail.com>
2024-10-04 14:29:46 -0700

    Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,9 @@
-export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: `${firstName}`,
-    lastName: `${lastName}`,
-  });
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
